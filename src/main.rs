962b2587c91bdd984d79d2a4fbb57be05fba1f31
Poffomoe <128525238+poffomoe@users.noreply.github.com>
2023-11-05 22:41:02 +1000

    did something very weird
    the app is now cross-platform, file with tasks is now being saved into home directory

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::fs::{File, read_to_string};
 use std::io::Write;
-use std::env::args;
-
-static PATH: &str = "todos";
+use std::env::{args, var_os};
 
 fn main() {
     println!("");
+
+    let binding = match var_os("HOME") {
+        Some(dir) => Some(dir),
+        None => match var_os("USERPROFILE") {
+            Some(dir) => Some(dir),
+            None => None
+        }
+    }.unwrap();
+    let home_dir = binding.to_str().unwrap();
+    let filename = format!("{}/todos", home_dir);
 
     let args: Vec<String> = args().collect();
     if args.len() == 1 {
@@ -13,34 +21,34 @@ fn main() {
         return;
     }
     match args[1].as_str() {
-        "a" | "add" => if args.len() > 2 {add_todo(args[2].clone())} else {println!("nothing to add. please write your task after \"a\"/\"add\"")},
-        "l" | "list" => list_todos(),
-        "d" | "delete" => if args.len() > 2 {delete_todo(args[2].clone())} else {println!("nothing to remove. please write task number after \"d\"/\"delete\"")},
+        "a" | "add" => if args.len() > 2 {add_todo(args[2].clone(), &filename)} else {println!("nothing to add. please write your task after \"a\"/\"add\"")},
+        "l" | "list" => list_todos(&filename),
+        "d" | "delete" => if args.len() > 2 {delete_todo(args[2].clone(), &filename)} else {println!("nothing to remove. please write task number after \"d\"/\"delete\"")},
         "h" | "help" => get_help(),
         _ => println!("unknown argument provided. use todo-cli-app help for help")
     }
 }
 
-fn get_vector() -> Vec<String> {
+fn get_vector(path: &str) -> Vec<String> {
     File::options()
         .read(true)
         .write(true)
         .create(true)
-        .open(PATH)
+        .open(path)
         .expect("couldn't create file");
     
-    read_to_string(PATH) 
+    read_to_string(path) 
         .unwrap()
         .lines()
         .map(String::from)  
         .collect()
 }
 
-fn write_file(contents: Vec<String>, message: String) -> () {
+fn write_file(contents: Vec<String>, message: String, path: &str) -> () {
     let mut f = File::options()
         .write(true)
         .truncate(true)
-        .open(PATH)
+        .open(path)
         .expect("cannot open file for writing");
 
     f.set_len(0)
@@ -53,16 +61,16 @@ fn write_file(contents: Vec<String>, message: String) -> () {
     println!("{}", message);
 }
 
-fn add_todo(task: String) {
-    let mut vector: Vec<String> = get_vector();
+fn add_todo(task: String, path: &str) {
+    let mut vector: Vec<String> = get_vector(path);
 
     vector.push(task.clone());
 
-    write_file(vector, format!("successfully added \"{}\" to the todo list", task));
+    write_file(vector, format!("successfully added \"{}\" to the todo list", task), path);
 }
 
-fn list_todos() {
-    let vector = get_vector();
+fn list_todos(path: &str) {
+    let vector = get_vector(path);
 
     let mut count = 0;
     if vector.len() == 0 {
@@ -76,8 +84,8 @@ fn list_todos() {
     };
 }
 
-fn delete_todo(index: String) {
-    let mut vector = get_vector();
+fn delete_todo(index: String, path: &str) {
+    let mut vector = get_vector(path);
 
     match index
             .trim()
@@ -90,7 +98,7 @@ fn delete_todo(index: String) {
                     
                     let tasktext: String = vector[number - 1].clone();
                     vector.remove(number - 1);
-                    write_file(vector, format!("successfully removed \"{}\" from the list", tasktext));
+                    write_file(vector, format!("successfully removed \"{}\" from the list", tasktext), path);
 
                 },
                 Err(_) => {
